c3ea5596d6613e529891be31563a9541d1248efb
中村昌暉 <smith@nakamuramasaakiranoMacBook-Pro.local>
2020-06-20 16:13:02 +0900

    Helpers Class and Function

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,9 +4,11 @@ import Vue from 'vue'
 import router from './Router/router.js'
 import axios from 'axios'
 import MainPage from './components/AppHome'
+import User from './Helpers/User'
 
 window.Vue = require('vue');
 window.axios = axios;
+window.User = User
 
 const app = new Vue({
     components: {
